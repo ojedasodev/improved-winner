84215354f160f6afc682957089b62931ddaa4184
ojedasodev <98894458+ojedasodev@users.noreply.github.com>
2024-03-22 09:42:34 -0500

    Revert "phone book backedn"
    This reverts commit 96870a98ea229143c1b0dc3fd6dd32e200e5f228.

--- Part3/phonebook/src/components/Input.jsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable react/prop-types */
-const Input = ({ value, onChange }) => {
-  return <input value={value} onChange={(e) => onChange(e, "filter")} />;
-};
-
-export default Input;
